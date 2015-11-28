cc1a0e9d52a3689a0a8f0e7866fcb8ffa78dfcce
Clayton Hottinger <chottinger@Claytons-MBP.PK5001Z>
2015-11-28 14:45:49 -0600

    Write functionality works, need to append comments to the dom. Also styling and such. Boo

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,13 @@
 $(function() {
-  console.log('I\'m here');
   $('form').on('submit', function(event) {
-    var newComment = $(this).serializeArray();
+    event.preventDefault();
+    var temp = $(this).serializeArray();
+    var newComment = {imageId: $(this).attr('class'),
+      message: temp[0].value};
     $.ajax({
-      type: 'post'
-      url: '/comments'
-      data: newComment[0]
+      type: 'post',
+      url: '/comments',
+      data: newComment
     }).done(function(data) {
 
     });
